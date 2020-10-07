7e8c8a7e881da0005d77701ea1eaed88d3979ac9
Nikita <main.acr0matic@gmail.com>
2020-10-07 20:38:30 +0300

    feat: сверстан футер

--- src/include/footer.php
+++ src/include/footer.php
@@ -1,5 +1,247 @@
   <!-- Подвал -->
-  <footer></footer>
+  <footer>
+    <div class="container">
+      <div class="foooter">
+        <div class="footer__row">
+          <div class="footer__navigation footer-navigation">
+            <div class="footer-navigation__row footer-navigation__row--underline">
+              <div class="footer-navigation__wrapper">
+                <div class="footer-navigation__section footer-navigation__section--margin footer-section">
+                  <h3 class="footer-section__title">Информация</h3>
+                  <div class="footer-section__wrapper">
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">О компании</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Контакты</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Доставка и монтаж</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Отзывы</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- /.footer-section__wrapper -->
+                </div>
+                <!-- /.footer-navigation_section -->
+
+                <div class="footer-navigation__section footer-section">
+                  <h3 class="footer-section__title">Кухни</h3>
+                  <div class="footer-section__wrapper">
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">ДСП</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">МДФ</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Эмаль</a>
+                      </li>
+                    </ul>
+                    <!-- /.footer-section__list -->
+
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Пленка</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Шпон</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Из акрила</a>
+                      </li>
+                    </ul>
+                    <!-- /.footer-section__list -->
+
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Хай тек</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Модерн</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Классика</a>
+                      </li>
+                    </ul>
+                    <!-- /.footer-section__list -->
+
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Пластик</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Массив Италия</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Российский ассив</a>
+                      </li>
+                    </ul>
+                    <!-- /.footer-section__list -->
+                  </div>
+                  <!-- /.footer-section__wrapper -->
+                </div>
+                <!-- /.footer-navigation_section -->
+              </div>
+              <!-- /.footer-navigation__wrapper -->
+            </div>
+            <!-- /.footer-navigation__row -->
+
+            <div class="footer-navigation__row">
+              <div class="footer-navigation__wrapper">
+                <div class="footer-navigation__section footer-navigation__section--margin footer-section">
+                  <h3 class="footer-section__title">Фурнитура</h3>
+                  <div class="footer-section__wrapper">
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Мойки</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Смесители</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Вытяжки</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- /.footer-section__wrapper -->
+                </div>
+                <!-- /.footer-navigation_section -->
+
+                <div class="footer-navigation__section footer-section">
+                  <h3 class="footer-section__title">Корпусная мебель</h3>
+                  <div class="footer-section__wrapper">
+                    <ul class="footer-section__list">
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Позиция 1</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Позиция 2</a>
+                      </li>
+
+                      <li class="footer-section__item">
+                        <a href="" class="footer-section__link link">Позиция 3</a>
+                      </li>
+                    </ul>
+                    <!-- /.footer-section__list -->
+                  </div>
+                  <!-- /.footer-section__wrapper -->
+                </div>
+                <!-- /.footer-navigation_section -->
+
+                <div class="footer-navigation__section footer-section footer-section--margin">
+                  <h3 class="footer-section__title">Мы в социальных сетях</h3>
+                  <div class="footer-section__social footer-social">
+                    <div class="footer-social__item">
+                      <a href="">
+                        <img src="img/icons/social/vk.svg" alt="">
+                      </a>
+                    </div>
+                    <!-- /.footer-section__social -->
+
+                    <div class="footer-social__item">
+                      <a href="">
+                        <img src="img/icons/social/facebook.svg" alt="">
+                      </a>
+                    </div>
+                    <!-- /.footer-section__social -->
+
+                    <div class="footer-social__item">
+                      <a href="">
+                        <img class="footer-social__icon" src="img/icons/social/instagram.svg" alt="">
+                      </a>
+                    </div>
+                    <!-- /.footer-section__social -->
+                  </div>
+                  <!-- /.footer-section__wrapper -->
+                </div>
+                <!-- /.footer-navigation_section -->
+              </div>
+              <!-- /.footer-navigation__wrapper -->
+            </div>
+            <!-- /.footer-navigation__row -->
+
+            <div class="footer-navigation__row">
+
+            </div>
+            <!-- /.footer-navigation__row -->
+          </div>
+          <!-- /.footer__navigation -->
+
+          <div class="footer__contact">
+            <div class="footer__logo-wrapper">
+              <img src="img/logo.svg" alt="" class="footer__logo">
+            </div>
+            <!-- /.footer__logo -->
+
+            <div class="footer-contact">
+              <div class="footer-contact__item">
+                <div class="footer-contact__title">
+                  Фабрика:
+                </div>
+
+                <div class="footer-contact__content">
+                  <a class="link" href="tel:+7 (495) 978-59-51">+7 (495) 978-59-51</a>
+                </div>
+                <!-- /.footer-contact__content -->
+              </div>
+              <!-- /.footer-contact__item -->
+
+              <div class="footer-contact__item">
+                <div class="footer-contact__title">
+                  Отделы <br> продаж:
+                </div>
+
+                <div class="footer-contact__content">
+                  <a class="link" href="tel:+7 (495) 978-59-51">+7 (495) 978-59-51</a>
+                  <a class="link" href="tel:+7 (495) 978-59-51">+7 (495) 978-59-51</a>
+                </div>
+                <!-- /.footer-contact__content -->
+              </div>
+              <!-- /.footer-contact__item -->
+
+              <div class="footer-contact__item">
+                <div class="footer-contact__title">
+                  Адрес:
+                </div>
+
+                <div class="footer-contact__content">
+                  Г. Москва, <br>
+                  Электролитный проезд. <br>
+                  дом 3 стр 2
+                </div>
+                <!-- /.footer-contact__content -->
+              </div>
+              <!-- /.footer-contact__item -->
+            </div>
+            <!-- /.footer-contact -->
+          </div>
+          <!-- /.footer__contact -->
+        </div>
+        <!-- /.footer__row -->
+      </div>
+      <!-- /.foooter -->
+    </div>
+    <!-- /.container -->
+  </footer>
 
   <!-- Скрипты -->
   <!-- build:js -->
