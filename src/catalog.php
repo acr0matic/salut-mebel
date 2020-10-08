e8ca0c6ca90fe26e71f98966a9115a05bc7f9f44
Nikita <main.acr0matic@gmail.com>
2020-10-08 14:28:09 +0300

    feat: сделана страница "Каталог"

---
+++ src/catalog.php
@@ -0,0 +1,229 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+
+
+  <section id="catalog">
+    <div class="container">
+      <div class="catalog">
+        <h2 class="catalog__title">
+          Каталог кухонь
+        </h2>
+
+        <div class="catalog__wrapper catalog__wrapper--wrap">
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/1.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/2.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/3.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/4.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/5.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/6.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/7.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/8.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/9.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/10.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/11.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+
+          <div class="catalog__item catalog-card catalog-card catalog-card--shadow">
+            <div class="catalog-card__picture">
+              <img src="img/catalog/12.jpg" alt="" class="catalog-card__image catalog-card__image--scale">
+            </div>
+            <!-- /.catalog-card__picture -->
+
+            <div class="catalog-card__wrapper">
+              <h3 class="catalog-card__title catalog-card__title--bold">
+                Кухня Элла
+              </h3>
+            </div>
+            <!-- /.catalog-card__wrapper -->
+
+            <a href="" class="stretched-link"></a>
+          </div>
+          <!-- /.catalog__item -->
+        </div>
+        <!-- /.catalog__wrapper -->
+      </div>
+      <!-- /.catalog -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once 'include/calculation.php' ?>
+
+</main>
+
+<?php include_once 'include/footer.php' ?>
