941d44fcf86ab2e755d049bda32578c573f8d055
Nikita <main.acr0matic@gmail.com>
2020-10-11 19:17:41 +0300

    feat: создан шаблон типового проекта

---
+++ src/solution.php
@@ -0,0 +1,102 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+  <section class="section">
+    <div class="container">
+      <h1 class="section__title">Кухни эмаль</h1>
+
+      <div class="product">
+        <div class="product__main">
+          <div class="product__picture">
+            <img src="img/product/solution/1.jpg" alt="" class="product__image">
+          </div>
+          <!-- /.product__picture -->
+          <div class="product__description">
+            <h2 class="product__title">Состав комплекта: </h2>
+            <ul class="product__list">
+              <li class="product__advantage">тумбы и навесные шкафы (h720)</li>
+              <li class="product__advantage">столешница (пластик, влагостойкая, 38мм)</li>
+              <li class="product__advantage">пристеночный бортик с заглушками</li>
+              <li class="product__advantage">цоколь, ручки</li>
+              <li class="product__advantage">сушка для посуды</li>
+              <li class="product__advantage">металобоксы на выкатные ящики</li>
+              <li class="product__advantage">петли с доводчиком (В ПОДАРОК!)</li>
+              <li class="product__advantage">навесы и мелкая фурнитура</li>
+            </ul>
+
+            <h2 class="product__title">Характеристики:</h2>
+            <ul class="product__list">
+              <li class="product__advantage">Фасад: </li>
+              <li class="product__advantage">Размер кухни: 2 400</li>
+              <li class="product__advantage">Цена погонного метра: </li>
+              <li class="product__advantage">Срок изготовления: 3-4 недели</li>
+            </ul>
+
+            <div class="product__action">
+              <div class="product__price">
+                Цена за погон. метр - от 43 850руб
+              </div>
+              <!-- /.product__price -->
+
+              <button class="product__button button button-secondary">
+                <span class="button-secondary__text">Рассчитать стоимость</span>
+              </button>
+            </div>
+            <!-- /.product__action -->
+          </div>
+          <!-- /.product__description -->
+        </div>
+        <!-- /.product__main -->
+      </div>
+      <!-- /.product -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="solutions" class="section">
+    <div class="container">
+      <h2 class="section__title section__title">
+        Другие типовые решения
+      </h2>
+
+      <div class="solution">
+        <div class="solution__wrapper flex-wrapper-fix">
+          <div class="solution__card solution-card">
+            <div class="solution-card__picture">
+              <img src="img/product/solution/2.jpg" alt="" class="solution-card__image">
+            </div>
+            <!-- /.olution-card__picture -->
+
+            <div class="solution-card__wrapper">
+              <h4 class="solution-card__title">Типовой проект 2</h4>
+              <button class="solution-card__button button button-primary">Посмотреть</button>
+            </div>
+            <!-- /.olution-card__wrapper -->
+          </div>
+          <!-- /.solution-card -->
+
+          <div class="solution__card solution-card">
+            <div class="solution-card__picture">
+              <img src="img/product/solution/3.jpg" alt="" class="solution-card__image">
+            </div>
+            <!-- /.solution-card__picture -->
+
+            <div class="solution-card__wrapper">
+              <h4 class="solution-card__title">Типовой проект 3</h4>
+              <button class="solution-card__button button button-primary">Посмотреть</button>
+            </div>
+            <!-- /.olution-card__wrapper -->
+          </div>
+          <!-- /.solution-card -->
+        </div>
+        <!-- /.solution__wrapper -->
+      </div>
+      <!-- /.solution -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once 'include/callback.php' ?>
+</main>
+
+<?php include_once 'include/footer.php' ?>
