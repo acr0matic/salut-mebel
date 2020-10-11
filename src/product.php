35c248ef4873da4881f1331faf8d0bf8c3755868
Nikita <main.acr0matic@gmail.com>
2020-10-11 15:47:18 +0300

    feat: верстка методики рассчета цены

--- src/product.php
+++ src/product.php
@@ -73,7 +73,7 @@
         <!-- /.product__main -->
 
         <div class="product__extra product-extra">
-          <div class="product-extra__wrapper">
+          <div class="product-extra__wrapper flex-wrapper-fix">
             <div class="product-extra__card">
               <h3 class="product-extra__title">Цветовые решения для эмалированных кухонь</h3>
               <div class="product-extra__picture">
@@ -95,7 +95,6 @@
               <button class="product-extra__button button button-primary">Посмотреть</button>
             </div>
             <!-- /.product-extra__card -->
-
           </div>
           <!-- /.product-extra__wrapper -->
         </div>
@@ -105,6 +104,72 @@
     </div>
     <!-- /.container -->
   </section>
+
+  <section id="method" class="section">
+    <div class="container">
+      <h2 class="section__title">Какая методика расчета цены <br> погонного метра в нашей компании?</h2>
+      <div class="method">
+        <div class="method__wrapper flex-wrapper-fix">
+          <div class="method__card method-card">
+            <div class="method-card__picture">
+              <img src="img/product/methods/1.jpg" alt="" class="method-card__image">
+            </div>
+            <!-- /.method-card__picture -->
+
+            <h3 class="method-card__title">В стоимость погонного метра входит:</h3>
+            <ul class="method-card__list">
+              <li class="method-card__item">тумба, 1 дверь 40см</li>
+              <li class="method-card__item">тумба, 1 дверь 60см</li>
+              <li class="method-card__item">навесной шкаф, 1 дверь 40см</li>
+              <li class="method-card__item">навесной шкаф, 1 дверь 60см</li>
+              <li class="method-card__item">навесной шкаф высотой 72см</li>
+            </ul>
+          </div>
+          <!-- /.method__card -->
+
+          <div class="method__card method-card">
+            <div class="method-card__picture">
+              <img src="img/product/methods/2.jpg" alt="" class="method-card__image">
+            </div>
+            <!-- /.method-card__picture -->
+
+            <h3 class="method-card__title">В стоимость кухни входит:</h3>
+            <ul class="method-card__list">
+              <li class="method-card__item">4 ручки</li>
+              <li class="method-card__item">петли с доводчиком</li>
+              <li class="method-card__item">ножки, цоколь</li>
+              <li class="method-card__item">пластиковая столешница</li>
+              <li class="method-card__item">навесы и крепежная фурнитура</li>
+            </ul>
+          </div>
+          <!-- /.method__card -->
+
+          <div class="method__card method-card">
+            <div class="method-card__picture">
+              <img src="img/product/methods/3.jpg" alt="" class="method-card__image">
+            </div>
+            <!-- /.method-card__picture -->
+
+            <h3 class="method-card__title">На итоговую стоимость влияют:</h3>
+            <ul class="method-card__list">
+              <li class="method-card__item">наличие ящиков</li>
+              <li class="method-card__item">выдвижные корзины</li>
+              <li class="method-card__item">фасады со стеклом</li>
+              <li class="method-card__item">радиусные элементы</li>
+            </ul>
+          </div>
+          <!-- /.method__card -->
+        </div>
+        <!-- /.method__wrapper -->
+      </div>
+      <!-- /.method -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section class="section"> </section>
+
+  <?php include_once 'include/calculation.php' ?>
 
   <?php include_once 'include/callback.php' ?>
 
