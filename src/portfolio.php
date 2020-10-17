61f54e573f9037813c777cf0cfa73712ffadf799
Nikita <main.acr0matic@gmail.com>
2020-10-17 23:04:31 +0300

    feat: свертана страница портфолио

---
+++ src/portfolio.php
@@ -0,0 +1,59 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+  <section class="section section--breadcrumbs">
+    <div class="container">
+      <ul class="section__breadcrumbs breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a href="" class="breadcrumbs__link link">Главная</a>
+        </li>
+
+        <li class="breadcrumbs__item breadcrumbs__item--active">портфолио</li>
+      </ul>
+
+      <div class="portfolio-block">
+        <div class="portfolio-block__wrapper flex-wrapper=fix">
+          <div class="portfolio-block__item">
+            <a data-fslightbox href="img/portfolio/portfolio-item.jpg">
+              <img src="img/portfolio/portfolio-item.jpg" alt="" class="portfolio__image">
+            </a>
+          </div>
+          <!-- /.portfolio-block__item -->
+
+          <div class="portfolio-block__item">
+            <a data-fslightbox href="img/portfolio/portfolio-item.jpg">
+              <img src="img/portfolio/portfolio-item.jpg" alt="" class="portfolio__image">
+            </a>
+          </div>
+          <!-- /.portfolio-block__item -->
+
+          <div class="portfolio-block__item">
+            <a data-fslightbox href="img/portfolio/portfolio-item.jpg">
+              <img src="img/portfolio/portfolio-item.jpg" alt="" class="portfolio__image">
+            </a>
+          </div>
+          <!-- /.portfolio-block__item -->
+
+          <div class="portfolio-block__item">
+            <a data-fslightbox href="img/portfolio/portfolio-item.jpg">
+              <img src="img/portfolio/portfolio-item.jpg" alt="" class="portfolio__image">
+            </a>
+          </div>
+          <!-- /.portfolio-block__item -->
+
+          <div class="portfolio-block__item">
+            <a data-fslightbox href="img/portfolio/portfolio-item.jpg">
+              <img src="img/portfolio/portfolio-item.jpg" alt="" class="portfolio__image">
+            </a>
+          </div>
+          <!-- /.portfolio-block__item -->
+        </div>
+        <!-- /.portfolio-block__wrapper -->
+      </div>
+      <!-- /.portfolio-block -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once 'include/footer.php' ?>
