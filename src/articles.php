58a058f3cd3efe7c94cf61702c25346386cc2b48
Nikita <main.acr0matic@gmail.com>
2020-10-17 18:42:07 +0300

    feat: верстка статей

---
+++ src/articles.php
@@ -0,0 +1,160 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+  <section class="section section--breadcrumbs">
+    <div class="container">
+      <ul class="section__breadcrumbs breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a href="" class="breadcrumbs__link link">Главная</a>
+        </li>
+
+        <li class="breadcrumbs__item breadcrumbs__item--active">статьи</li>
+      </ul>
+      <h2 class="section__title">Статьи</h2>
+
+      <div class="article">
+        <div class="article__wrapper article__wrapper--wrap flex-wrapper-fix">
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-1.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-2.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-3.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Основные модные тенденции в кухнях?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-4.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-1.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-1.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+          <article class="article__item">
+            <div class="article__picture">
+              <a href="">
+                <img src="img/articles/article-1.jpg" alt="" class="article__image">
+              </a>
+            </div>
+            <!-- /.article__picture -->
+
+
+            <div class="article__content">
+              <h3 class="article__title">Как собрать кухню самому?</h3>
+              <time class="article__date" datetime="2020-08-28">28.08.2020</time>
+              <button class="article__button button button-primary">Подробнее</button>
+            </div>
+            <!-- /.article__content -->
+
+          </article>
+          <!-- /.article__item -->
+
+
+        </div>
+        <!-- /.swiper-container -->
+      </div>
+      <!-- /.articles -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once 'include/footer.php' ?>
