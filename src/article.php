f1365fe968b48484310ff90027598d73f2c48f5c
Nikita <main.acr0matic@gmail.com>
2020-10-17 22:37:39 +0300

    feat: верстка страницы статьи

---
+++ src/article.php
@@ -0,0 +1,52 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+
+  <section class="section section--breadcrumbs">
+    <div class="container">
+      <ul class="section__breadcrumbs breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a href="" class="breadcrumbs__link link">Главная</a>
+        </li>
+
+        <li class="breadcrumbs__item">
+          <a href="" class="breadcrumbs__link link">статьи</a>
+        </li>
+
+        <li class="breadcrumbs__item breadcrumbs__item--active">Основные модные тенденции в кухнях</li>
+      </ul>
+    </div>
+    <!-- /.container -->
+
+    <div class="container container--tight">
+      <div class="article-block">
+        <div class="article-block__picture block-skewed">
+          <img src="img/articles/picture-1.jpg" alt="" class="article-block__image">
+        </div>
+        <!-- /.article-block__picture -->
+        <h2 class="article-block__title section__title">Основные модные тенденции в кухнях</h2>
+        <h3 class="article-block__subtitle">Краткое описание</h3>
+
+        <h4 class="article-block__heading">Заголовок</h4>
+        <p class="article-block__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+        <p class="article-block__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+        <p class="article-block__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+        <div class="article-block__picture">
+          <img src="img/articles/picture-2.jpg" alt="" class="article-block__image">
+        </div>
+        <!-- /.article-block__picture -->
+
+        <h4 class="article-block__heading">Заголовок</h4>
+        <p class="article-block__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+      </div>
+      <!-- /.article-block -->
+
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once 'include/articles.php' ?>
+
+</main>
+
+<?php include_once 'include/footer.php' ?>
