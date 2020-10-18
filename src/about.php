f13b082ecbee37c7fc07dd27dcdbb9eeb6ea6d1c
Nikita <main.acr0matic@gmail.com>
2020-10-18 16:18:10 +0300

    feat: верстка страницы "о нас"

---
+++ src/about.php
@@ -0,0 +1,99 @@
+<?php include_once 'include/header.php' ?>
+
+<main>
+
+  <section class="section section--breadcrumbs">
+    <div class="container">
+      <ul class="section__breadcrumbs breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a href="" class="breadcrumbs__link link">Главная</a>
+        </li>
+
+        <li class="breadcrumbs__item breadcrumbs__item--active">о нас</li>
+      </ul>
+
+      <div class="about">
+        <div class="about__header">
+          <div class="about__picture about__picture--main block-skewed">
+            <img src="img/about/header-image.jpg" alt="" class="about__image">
+            <h2 class="about__title">
+              О фабрике
+              <br>
+              "Салют-мебель"
+            </h2>
+          </div>
+          <!-- /.about__picture -->
+
+          <div class="about__information block-skewed">
+            <p class="about__text">
+              «САЛЮТ МЕБЕЛЬ » – фабрика кухни по производству удобных
+              и красивых гарнитуров. Товары предназначены широкому кругу потребителей с разным уровнем доходов.
+            </p>
+
+            <p class="about__text">
+              В ассортименте представлены как классические, так
+              и современные модели. Фабрика «САЛЮТ МЕБЕЛЬ»
+              - это уникальный производственно-складской комплекс, расположенный в Москве.
+            </p>
+          </div>
+          <!-- /.about__description -->
+        </div>
+        <!-- /.about__header -->
+
+        <div class="about__content">
+          <div class="about__row">
+            <div class="about__description">
+              <p class="about__text">
+                Фабрика «Салют Мебель» предлагает приобрести кухни
+                от производителя в Москве и Московской области через
+                сеть фирменных салонов.
+              </p>
+
+              <p class="about__text">
+                Для заказа доступна как мебель из готовых коллекций, так
+                и дизайнерские гарнитуры. Вы можете купить кухню на заказ
+                с гарантией производителя сроком 1 год.
+              </p>
+            </div>
+            <!-- /.about__description -->
+
+            <div class="about__picture">
+              <img src="img/about/1.jpg" alt="" class="about__image">
+            </div>
+            <!-- /.about__picture -->
+          </div>
+          <!-- /.about__row -->
+
+          <div class="about__row">
+            <div class="about__picture">
+              <img src="img/about/2.jpg" alt="" class="about__image">
+            </div>
+            <!-- /.about__picture -->
+
+            <div class="about__description">
+              <p class="about__text">
+                Фабрика «Салют Мебель» предлагает приобрести кухни
+                от производителя в Москве и Московской области через
+                сеть фирменных салонов.
+              </p>
+
+              <p class="about__text">
+                Для заказа доступна как мебель из готовых коллекций, так
+                и дизайнерские гарнитуры. Вы можете купить кухню на заказ
+                с гарантией производителя сроком 1 год.
+              </p>
+            </div>
+            <!-- /.about__description -->
+          </div>
+          <!-- /.about__row -->
+        </div>
+        <!-- /.about__content -->
+      </div>
+      <!-- /.about -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+</main>
+
+<?php include_once 'include/footer.php' ?>
