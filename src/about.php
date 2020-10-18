07a8732d171023125237fb86e27113d5ba82cd40
Nikita <main.acr0matic@gmail.com>
2020-10-18 16:52:14 +0300

    feat: сверстана секция "почему мы?

--- src/about.php
+++ src/about.php
@@ -94,6 +94,149 @@
     <!-- /.container -->
   </section>
 
+  <section class="section">
+    <div class="container">
+      <h2 class="section__title">Почему нам доверяют</h2>
+      <div class="reason">
+        <div class="reason__row">
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/fix.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Собственное производство в Москве</h3>
+              <p class="reason-card__text">
+                Фабрика «САЛЮТ МЕБЕЛЬ»
+                является производителем кухонных гарнитуров
+                и корпусной мебели с 15-тилетним опытом работы
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/product.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Полная комплектация </h3>
+              <p class="reason-card__text">
+                Вместе с кухонным гарнитуром
+                Вы можете приобрести технику,
+                мойку, смеситель, а так же
+                дополнительную фурнитуру
+                и освещение
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/measurement.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Замер и консультация с дизайнером</h3>
+              <p class="reason-card__text">
+                Замерщик выезжает на адрес
+                в любое удобное для Вас время
+                и производит замер помещения
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/kitchen-1.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Кухонные гарнитуры по цене производителя</h3>
+              <p class="reason-card__text">
+                Наша фабрика изготавливает
+                кухни любых конфигураций
+                по размерам заказчика
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+        </div>
+        <!-- /.reason__row -->
+
+        <div class="reason__row reason__row--center">
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/delivery.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Доставка и установка в 1 день</h3>
+              <p class="reason-card__text">
+                Вы принимаете уже собранную
+                кухню, после проверки
+                ее качества и работоспособности
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+
+          <div class="reason__card reason__card--margin reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/kitchen-2.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Кухни под любые размеры</h3>
+              <p class="reason-card__text">
+                Наша фабрика изготавливает
+                кухни любых конфигураций
+                по размерам заказчика
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+
+          <div class="reason__card reason-card">
+            <div class="reason-card__wrapper">
+              <div class="reason-card__icon">
+                <img src="img/icons/market.svg" alt="">
+              </div>
+              <!-- /.reason-card__icon -->
+
+              <h3 class="reason-card__title">Сеть фирменных салонов</h3>
+              <p class="reason-card__text">
+                Нашу продукцию можно приобрести только
+                в салонах “САЛЮТ МЕБЕЛЬ”, без
+                магазинных наценок
+              </p>
+            </div>
+            <!-- /.reason-card__wrapper -->
+          </div>
+          <!-- /.reason__card -->
+        </div>
+        <!-- /.reason__row -->
+      </div>
+      <!-- /.reason -->
+    </div>
+    <!-- /.container -->
+  </section>
+
 </main>
 
 <?php include_once 'include/footer.php' ?>
