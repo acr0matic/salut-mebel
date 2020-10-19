a0bec0725d690cdb5b98bfb6503963dcefc09aef
Nikita <main.acr0matic@gmail.com>
2020-10-20 00:34:52 +0300

    feat: адаптив футера

--- src/include/footer.php
+++ src/include/footer.php
@@ -1,97 +1,113 @@
   <!-- Подвал -->
   <footer>
     <div class="container">
-      <div class="foooter">
+      <div class="footer">
         <div class="footer__row">
           <div class="footer__navigation footer-navigation">
             <div class="footer-navigation__row footer-navigation__row--underline">
               <div class="footer-navigation__wrapper">
-                <div class="footer-navigation__section footer-navigation__section--margin footer-section">
+                <div class="footer-navigation__section footer-navigation__section--margin footer-section footer-section--underline">
                   <h3 class="footer-section__title">Информация</h3>
                   <div class="footer-section__wrapper">
-                    <ul class="footer-section__list">
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">О компании</a>
-                      </li>
+                    <div class="footer-section__list footer-list">
+                      <div class="footer-list__wrapper">
+                        <div class="footer-list__column">
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">О компании</a>
+                          </div>
+                          <!-- /.footer-list__item -->
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Контакты</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Контакты</a>
+                          </div>
+                          <!-- /.footer-list__item -->
+                        </div>
+                        <!-- /.footer-list__row -->
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Доставка и монтаж</a>
-                      </li>
+                        <div class="footer-list__column">
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Отзывы</a>
+                          </div>
+                          <!-- /.footer-list__item -->
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Отзывы</a>
-                      </li>
-                    </ul>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Доставка и монтаж</a>
+                          </div>
+                          <!-- /.footer-list__item -->
+                        </div>
+                        <!-- /.footer-list__row -->
+                      </div>
+                      <!-- /.footer-list__wrapper -->
+                    </div>
+                    <!-- /.footer-section__wrapper -->
                   </div>
-                  <!-- /.footer-section__wrapper -->
+                  <!-- /.footer-navigation_section -->
                 </div>
-                <!-- /.footer-navigation_section -->
 
-                <div class="footer-navigation__section footer-section">
+                <div class="footer-navigation__section">
                   <h3 class="footer-section__title">Кухни</h3>
                   <div class="footer-section__wrapper">
-                    <ul class="footer-section__list">
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">ДСП</a>
-                      </li>
+                    <div class="footer-section__list footer-list">
+                      <div class="footer-list__wrapper footer-list__wrapper--flex">
+                        <div class="footer-list__column">
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">ДСП</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">МДФ</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Пленка</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Эмаль</a>
-                      </li>
-                    </ul>
-                    <!-- /.footer-section__list -->
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Хай тек</a>
+                          </div>
 
-                    <ul class="footer-section__list">
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Пленка</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Пластика</a>
+                          </div>
+                        </div>
+                        <!-- /.footer-list__row -->
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Шпон</a>
-                      </li>
+                        <div class="footer-list__column">
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">МДФ</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Из акрила</a>
-                      </li>
-                    </ul>
-                    <!-- /.footer-section__list -->
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Шпон</a>
+                          </div>
 
-                    <ul class="footer-section__list">
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Хай тек</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Модерн</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Модерн</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Массив Италия</a>
+                          </div>
+                        </div>
+                        <!-- /.footer-list__row -->
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Классика</a>
-                      </li>
-                    </ul>
-                    <!-- /.footer-section__list -->
+                        <div class="footer-list__column">
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Эмаль</a>
+                          </div>
 
-                    <ul class="footer-section__list">
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Пластик</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Из акрила</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Массив Италия</a>
-                      </li>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Классика</a>
+                          </div>
 
-                      <li class="footer-section__item">
-                        <a href="" class="footer-section__link link">Российский ассив</a>
-                      </li>
-                    </ul>
+                          <div class="footer-list__item">
+                            <a href="" class="footer-list__link link">Российский массив</a>
+                          </div>
+                        </div>
+                        <!-- /.footer-list__row -->
+                      </div>
+                      <!-- /.footer-list__wrapper -->
+                    </div>
                     <!-- /.footer-section__list -->
                   </div>
                   <!-- /.footer-section__wrapper -->
@@ -147,19 +163,19 @@
                 </div>
                 <!-- /.footer-navigation_section -->
 
-                <div class="footer-navigation__section footer-section footer-section--margin">
+                <div class="footer-navigation__section footer-navigation__section--hide footer-section footer-section--margin">
                   <h3 class="footer-section__title">Мы в социальных сетях</h3>
                   <div class="footer-section__social footer-social">
                     <div class="footer-social__item">
                       <a href="">
-                        <img src="img/icons/social/vk.svg" alt="">
+                        <img class="footer-social__icon" src="img/icons/social/vk.svg" alt="">
                       </a>
                     </div>
                     <!-- /.footer-section__social -->
 
                     <div class="footer-social__item">
                       <a href="">
-                        <img src="img/icons/social/facebook.svg" alt="">
+                        <img class="footer-social__icon" src="img/icons/social/facebook.svg" alt="">
                       </a>
                     </div>
                     <!-- /.footer-section__social -->
@@ -176,11 +192,6 @@
                 <!-- /.footer-navigation_section -->
               </div>
               <!-- /.footer-navigation__wrapper -->
-            </div>
-            <!-- /.footer-navigation__row -->
-
-            <div class="footer-navigation__row">
-
             </div>
             <!-- /.footer-navigation__row -->
           </div>
