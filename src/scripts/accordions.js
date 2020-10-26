6bb3e86c75617ed50ea9f541cdb8de9a1d542449
Nikita <main.acr0matic@gmail.com>
2020-10-26 19:42:05 +0300

    refactor: чутка подкорректирован скрипт аккордеона

--- src/scripts/accordions.js
+++ src/scripts/accordions.js
@@ -4,14 +4,14 @@
 const accordions = Array.from(document.querySelectorAll('.handorgel')).map((selector) => new handorgel(selector));
 
 accordions.forEach((accordion) => {
+  const icon = accordion.element.querySelector('.handorgel__icon');
+
   accordion.on('fold:open', () => {
-    const icon = accordion.element.querySelector('.handorgel__icon');
     icon.classList.toggle('handorgel__icon--opened');
   });
 
   // listen for event
   accordion.on('fold:close', () => {
-    const icon = accordion.element.querySelector('.handorgel__icon');
     icon.classList.toggle('handorgel__icon--opened');
   });
 });
