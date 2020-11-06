b149aef6664fc85186228fa84d5414c4558eb0f5
Nikita <main.acr0matic@gmail.com>
2020-11-06 15:10:32 +0300

    feat: убрана минификация php для удобной натяжки

--- gulp_tasks/html.js
+++ gulp_tasks/html.js
@@ -22,7 +22,7 @@ task('html', () => src(paths.html.src)
     htmlMin({
       sortAttributes: true,
       sortClassName: true,
-      collapseWhitespace: true,
+      collapseWhitespace: false, // Поставить true, если нужно минифицировать
     }),
   )
   .pipe(debug({
